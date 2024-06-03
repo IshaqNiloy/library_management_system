c1de4c14547573154e56f8f6c8c447f2e406d859
IshaqNiloy <ishaqniloy1996@gmail.com>
2024-06-04 04:44:38 +0600

    initiated search api

--- book/admin.py
+++ book/admin.py
@@ -4,6 +4,6 @@ from .models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'publisher')
-    search_fields = ('title', 'author', 'publisher')
-    list_filter = ('publisher', 'author')
+    list_display = ('title', 'author', 'genre', 'edition', 'country', 'language', 'publisher')
+    search_fields = ('title', 'author', 'isbn', 'publisher')
+    list_filter = ('publisher', 'author', 'genre', 'country', 'language')
--- book/views.py
+++ book/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from typing import Union, List
 
-# Create your views here.
+from rest_framework.views import APIView
+
+from book.models import Book
+
+
+class BookView(APIView):
+    def __init__(self):
+        super().__init__()
+        self.model = Book
+
+    def search(self, title: Union[str, None] = None, author: Union[str, None] = None,
+               genre: Union[str, None] = None, country: Union[str, None] = None,
+               language: Union[str, None] = None, isbn: Union[str, None] = None,
+               publisher: Union[str, None] = None) -> List[Book]:
+        if title:
+            return self.model.objects.search_book()
+        if author:
+            return self.model.objects.search_book_author()
+
+    def search(self):
+
+    def get(self, request):
+
